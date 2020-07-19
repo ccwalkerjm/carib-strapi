a7183998ee9ecdb09ed71156760a0185286b2821
Courtney Walker <ccwalkerjm@gmail.com>
2020-07-19 04:48:52 -0500

    setup carib strapi

---
+++ config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT, 10),
+        database: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        ssl: process.env.DATABASE_SSL.toLowerCase() === "true",
+      },
+      options: {},
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7e49f602-4426-416c-889e-bf752266b0e5'
+};
